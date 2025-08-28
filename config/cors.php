cf41b59e1623b376addefc8a4fa989eca091bc07
Suong Khonmarady <maradybor@gmail.com>
2025-08-28 10:24:06 +0700

    url

--- config/cors.php
+++ config/cors.php
@@ -29,7 +29,7 @@ return [
         'http://127.0.0.1:8000',
         'http://127.0.0.1:8080',
         env('FRONTEND_URL', 'http://localhost:3000'),
-        'https://svrwu.vercel.app/',
+        'https://svrwu.vercel.app',
     ],
 
     'allowed_origins_patterns' => [],
